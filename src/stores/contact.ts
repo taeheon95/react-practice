8c886ed407413718fe72bb5390b6f439b62778e6
taeheon95 <dalo952003@gmail.com>
2021-09-01 11:17:50 +0900

    delete not working recoil function on contact

--- src/stores/contact.ts
+++ src/stores/contact.ts
@@ -1,6 +1,6 @@
 import { getAllContact } from "../apis/contactAPI";
 import { Contact, EditContact } from "../types/Contact";
-import { atom, selector, selectorFamily } from "recoil";
+import { atom } from "recoil";
 
 export const contactListState = atom<Contact[]>({
   key: "contactList",
@@ -15,23 +15,6 @@ export const editContactState = atom<EditContact>({
 export const selectedContactState = atom<Contact | null>({
   key: "selectedContactState",
   default: null,
-});
-
-export const editContactSelector = selectorFamily<EditContact, string>({
-  key: "editContactSelector",
-  get:
-    (field: string) =>
-    ({ get }) => {
-      const contact: any = { ...get(editContactState) };
-      return contact[field];
-    },
-  set:
-    (field: string) =>
-    ({ set }, newValue) =>
-      set(editContactState, (prevState) => ({
-        ...prevState,
-        [field]: newValue,
-      })),
 });
 
 export const keywordState = atom<string>({
