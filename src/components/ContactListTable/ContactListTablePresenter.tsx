8bb12a15c93b93a8ba6341423f217a0e038c7772
taeheon95 <dalo952003@gmail.com>
2021-09-01 11:18:35 +0900

    make contactlist table

--- src/components/ContactListTable/ContactListTablePresenter.tsx
+++ src/components/ContactListTable/ContactListTablePresenter.tsx
@@ -1,7 +1,78 @@
+import { Button, Table } from "antd";
 import React from "react";
+import { ContactItem } from "../../types/Contact";
+import { ColumnsType } from "antd/es/table";
 
-function ContactListTablePresenter() {
-  return <div></div>;
-}
+const ContactListTablePresenter = ({
+  contactList,
+  deleteContact,
+}: {
+  contactList: ContactItem[];
+  deleteContact: Function;
+}) => {
+  const columns: ColumnsType<ContactItem> = [
+    {
+      title: "이름",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "나이",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "이메일",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "전화번호",
+      dataIndex: "phoneNumber",
+      key: "email",
+    },
+    {
+      title: "설명",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Actions",
+      key: "action",
+      render: (text, record) => (
+        <>
+          <Button type="link" onClick={(e) => {}}>
+            update
+          </Button>
+          &nbsp;
+          <Button
+            type="link"
+            onClick={(e) => {
+              deleteContact(record.key);
+            }}
+            danger
+          >
+            delete
+          </Button>
+        </>
+      ),
+    },
+  ];
+  return (
+    <>
+      <div style={{ marginBottom: "16px" }}>
+        <Button type="link">Add</Button>
+        <Button type="link" danger>
+          delete
+        </Button>
+      </div>
+      <Table
+        columns={columns}
+        dataSource={contactList}
+        pagination={{ position: ["bottomRight"] }}
+      />
+    </>
+  );
+};
 
 export default ContactListTablePresenter;
